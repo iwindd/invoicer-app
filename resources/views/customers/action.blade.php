72797bcd874cfd0309b34a2ef4d8fb23102114c4
iwindd <freewinendlife@gmail.com>
2024-06-23 20:00:41 +0700

    feat: locale

--- resources/views/customers/action.blade.php
+++ resources/views/customers/action.blade.php
@@ -1,10 +1,10 @@
 <div class="dropdown show ">
   <a class="btn btn-primary dropright btn-sm sm-3 shadow-sm dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    เพิ่มเติม
+    {{__('ui.more')}}
   </a>
 
   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <a class="dropdown-item" href="{{route("customer", ['id' => $id])}}"><i class="fas fa-info-circle mr-2 text-info"></i>รายละเอียด</a>
-    <a class="dropdown-item delete" href="#" onclick="delFunc({{$id}}, '{{$firstname}} {{$lastname}}')"><i class="fas fa-trash mr-2 text-danger"></i>ลบ</a>
+    <a class="dropdown-item" href="{{route("customer", ['id' => $id])}}"><i class="fas fa-info-circle mr-2 text-info"></i>{{__('ui.details')}}</a>
+    <a class="dropdown-item delete" href="#" onclick="delFunc({{$id}}, '{{$firstname}} {{$lastname}}')"><i class="fas fa-trash mr-2 text-danger"></i>{{__('ui.delete-btn')}}</a>
   </div>
 </div>
