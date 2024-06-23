86ee878a490847876c0d45b19958dacac70172ad
iwindd <freewinendlife@gmail.com>
2024-06-23 20:00:54 +0700

    fix: locale not load

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -183,7 +183,45 @@
   <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
   <script src="{{ asset('js/dayjs.min.js') }}"></script>
   <script src="{{ asset('js/validation.js') }}"></script>
-  <script src="{{ asset('js/alert.js') }}"></script>
+  <script>
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "bottom-end",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    });
+
+    const Confirmation = Swal.mixin({
+      title: "{{ __('ui.dialogHeader') }}",
+      customClass: {
+        confirmButton: "btn shadow-sm btn-primary mr-1",
+        cancelButton: "btn shadow-sm btn-secondary ml-1",
+        icon: "text-warning border-warning"
+      },
+      showCancelButton: true,
+      buttonsStyling: false,
+      cancelButtonText: "{{ __('ui.dialogCancel') }}",
+      confirmButtonText: "{{ __('ui.dialogConfirm') }}",
+      icon: "warning",
+    });
+
+    const Alert = {
+      success: Swal.mixin({
+        title: "{{ __('ui.dialogHeaderSuccess') }}",
+        customClass: {
+          confirmButton: "btn shadow-sm btn-primary mr-1",
+        },
+        buttonsStyling: false,
+        confirmButtonText: "{{ __('ui.dialogConfirm') }}",
+        icon: "success",
+      })
+    }
+  </script>
 
   <script type="text/javascript">
     $(document).ready(function() {
