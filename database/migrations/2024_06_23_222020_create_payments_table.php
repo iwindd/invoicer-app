7572f284cf715a0ec54738a5b9e9b0cb88f0c751
iwindd <freewinendlife@gmail.com>
2024-06-24 05:22:15 +0700

    feat: Payment Model

---
+++ database/migrations/2024_06_23_222020_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('application');
+            $table->text('title');
+            $table->text('name');
+            $table->text('account');
+            $table->boolean('active');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
