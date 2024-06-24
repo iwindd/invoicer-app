657194a1ab51714c7461ce77684f1f3b7416ee2f
iwindd <freewinendlife@gmail.com>
2024-06-24 17:05:07 +0700

    feat: action guard

--- resources/views/payments/action.blade.php
+++ resources/views/payments/action.blade.php
@@ -5,7 +5,9 @@
 
   <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
     <button class="dropdown-item" onclick="editFunc({{$id}}, '{{$title}}', '{{$account}}', '{{$name}}')"><i class="fas fa-pen fa-fw mr-2 text-primary"></i>{{ __('ui.edit-btn') }}</button>
-    <button class="dropdown-item" onclick="delFunc({{$id}}, '{{$title}}: {{$account}}')"><i class="fas fa-trash fa-fw mr-2 text-danger"></i>{{ __('ui.delete-btn') }}</button>
-    <button class="dropdown-item" onclick="patchFunc({{$id}}, '{{$title}}: {{$account}}')"><i class="fas fa-toggle-on fa-fw mr-2 text-info"></i>{{ __('payment.use2') }}</button>
+    @if (!$active)
+      <button class="dropdown-item" onclick="delFunc({{$id}}, '{{$title}}: {{$account}}')"><i class="fas fa-trash fa-fw mr-2 text-danger"></i>{{ __('ui.delete-btn') }}</button>
+      <button class="dropdown-item" onclick="patchFunc({{$id}}, '{{$title}}: {{$account}}')"><i class="fas fa-toggle-on fa-fw mr-2 text-info"></i>{{ __('payment.use2') }}</button>
+    @endif
   </div>
 </div>
