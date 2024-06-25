867e6ef648e34adc0cbca1cbc7a86da534016b5e
iwindd <freewinendlife@gmail.com>
2024-06-25 10:18:16 +0700

    feat: handler error

--- resources/lang/th/activity.php
+++ resources/lang/th/activity.php
@@ -2,5 +2,9 @@
 
 return [
   "profile-update" => "แก้ไขข้อมูลผู้ใช้เรียบร้อยแล้ว! (:name)",
-  "profile-password-patch" => "เปลี่ยนรหัสผ่านเรียบร้อยแล้ว!"
+  "profile-password-patch" => "เปลี่ยนรหัสผ่านเรียบร้อยแล้ว!",
+
+  "customer-create" => "เพิ่มลูกค้า :firstname :lastname แล้ว!",
+  "customer-update" => "แก้ไขลูกค้า :firstname :lastname แล้ว!",
+  "customer-delete" => "ลบลูกค้า :firstname :lastname แล้ว!",
 ];
