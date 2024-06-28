17de07fef4bdc8e288807f22fc1369ec61613d06
iwindd <freewinendlife@gmail.com>
2024-06-28 23:44:10 +0700

    chore: show name on phone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,7 +94,7 @@
             <li class="nav-item dropdown ">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
+                <span class="mr-2 text-gray-600 small">{{ Auth::user()->name }}</span>
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
